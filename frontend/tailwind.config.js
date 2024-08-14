7b405c0dd5634d4e0a43d2b8031694708994c79a
g4orge <145166271+g4orge@users.noreply.github.com>
2024-08-14 19:33:53 -0400

    front end files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust according to your file types and paths
+  ],
+  theme: {
+    extend: {
+      colors: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
