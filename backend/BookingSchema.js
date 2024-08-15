95e3e7faa981750ff760fe4e5634e49ab89dd269
g4orge <145166271+g4orge@users.noreply.github.com>
2024-08-15 18:29:27 -0400

    na

--- backend/BookingSchema.js
+++ backend/BookingSchema.js
@@ -1,14 +1,33 @@
 import mongoose from "mongoose";
 
-const BookingSchema = new mongoose.Schema({
-    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
-    doctor: { type: mongoose.Types.ObjectId, ref: "Doctor", required: true },
-    date: { type: Date, required: true },
-    timeSlot: { type: String, required: true },
-    status: { type: String, enum: ['Pending', 'Confirmed', 'Cancelled'], default: 'Pending' },
-    paymentStatus: { type: String, enum: ['Pending', 'Paid'], default: 'Pending' },
-    ticketPrice: { type: Number, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
+const bookingSchema = new mongoose.Schema(
+  {
+    doctor: {
+      type: mongoose.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    ticketPrice: { type: String, required: true },
+    appointmentDate: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "cancelled"],
+      default: "pending",
+    },
+    isPaid: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("Booking", BookingSchema);
+export default mongoose.model("Booking", bookingSchema);
