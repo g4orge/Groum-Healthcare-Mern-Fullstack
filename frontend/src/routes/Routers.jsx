8564c32c2d0746176c66c3ec6e6c02abf55c5194
g4orge <145166271+g4orge@users.noreply.github.com>
2024-08-15 20:08:39 -0400

    jsx files

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,5 +1,25 @@
 import React from "react";
-const Router = () => {
-    return <div>Router</div>;
+import Home from '../pages/Home'
+import Services from '../pages/Services'
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+import Contact from '../pages/Contact'
+import Doctors from '../pages/Doctors/Doctors'
+import DoctorDetails from '../pages/Doctors/DoctorDetails'
+
+import {Routes, Route} from 'react-router-dom'
+
+const Routers = () => {
+    return <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/doctors" element={<Doctors/>} />
+        <Route path="/doctors/:id" element={<DoctorDetails/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Signup/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/Services" element={<Services/>} />
+        
+    </Routes>
 };
 export default Routers;
